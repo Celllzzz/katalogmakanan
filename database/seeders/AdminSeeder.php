ac93962a30b51441042a148d8ca1a290a00a4fad
Celllzzz <Celllzzz>
2025-04-27 14:09:15 +0700

    membuat adminseeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    
+    public function run(): void
+    {
+        Admin::create([
+            'name' => 'Super Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('admin123'),
+        ]);
+    }
+}
